20eaafa2166db4c148fe10ecfc42b671e17f1d8b
vagrant <vagrant@vagrant.vm>
2017-11-06 23:23:32 +0000

    finished planning

--- josephus.py
+++ josephus.py
@@ -13,10 +13,20 @@ As a sanity case, if never skip anyone, the last person will be our survivor:
 
 """
 
+# Plan:
+    # Need a circular linked list (last item circles back to beginning)
+    # Actions:
+        # Add node - use 1st param to initialize LL
+        # Traversal - use 2nd param to know how many times to jump
+        # Remove node
+    # Considerations:
+        # Do we need to remove node? Or just find a way to mark it?
+        # Circular traversal ends when LL only has 1 node left?
 
 
 def find_survivor(num_people, kill_every):
     """Given num_people in circle, kill [kill_every]th person, return survivor."""
+
 
 
 if __name__ == '__main__':
