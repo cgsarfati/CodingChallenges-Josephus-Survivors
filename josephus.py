4c6c75f4fdfd32712a7443c8ada02bfed4b78b5f
vagrant <vagrant@vagrant.vm>
2017-11-07 01:04:24 +0000

    solved problem

--- josephus.py
+++ josephus.py
@@ -34,9 +34,27 @@ class Node(object):
 
     @classmethod
     def make_list(cls, n):
-        """Construct circular doubly-linked list of n items. Returns head node."""
+        """Construct circular doubly-linked list of n items. Returns head node.
 
-        # Make 1st node (+ remember that it's the first for end output)
+            >>> node = Node.make_list(3)
+
+            >>> node.data
+            1
+
+            >>> node.next.data
+            2
+
+            >>> node.next.next.next.data
+            1
+
+            >>> node.prev.data
+            3
+
+            >>> node.prev.prev.prev.data
+            1
+        """
+
+        # Make 1st node (+ remember that it's the first for later)
             # Side-note: can do multiple-assignments in python!
         first = node = prev = cls(1)
 
@@ -52,6 +70,33 @@ class Node(object):
 
         # Return head node
         return first
+
+
+def find_survivor(num_people, kill_every):
+    """Given num_people in circle, kill [kill_every]th person, return survivor."""
+
+    # Initialize CLS, return head node for starting the traversal
+    node = Node.make_list(num_people)
+
+    # Loop until only 1 item left in lst (= survivor!)
+    while node.next != node:
+
+        # Land on node to remove
+            # -1 bc we start on 1st node. If 3rd node, only jump 2x.
+        for i in range(kill_every - 1):
+            node = node.next
+
+        # We've landed on node to remove. Remove.
+        # Here, we essentially re-attach the prev and next nodes
+            # together to rid of current node.
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        # Move to next node for next jump
+        node = node.next
+
+    # Return survivor's data
+    return node.data
 
 
 if __name__ == '__main__':
